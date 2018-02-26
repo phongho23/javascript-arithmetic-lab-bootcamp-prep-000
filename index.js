c2d15eb4cbdcb3f703a815c23532f05238d4eea4
Phong Ho <ho.phongt@gmail.com>
2018-02-26 06:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,18 +33,10 @@ function makeInt(n) {
   return (parseInt(n,10))
 }
 
-function makeInt(n) {
-  if (n ===)
+function isNaN(n) {
+
 }
 
-ribe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
