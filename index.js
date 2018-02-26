3d5e06214ad4b19dcafe45255ad87187502cc45f
Phong Ho <ho.phongt@gmail.com>
2018-02-26 06:14:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,22 +33,7 @@ function makeInt(n) {
   return (parseInt(n,10))
 }
 
-function isNaN(n) {
-
+function preserveDecimal(n) {
+  return (parseFloat(n))
 }
 
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
